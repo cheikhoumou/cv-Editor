f04d704baefc64d63b351a1a13357063ea219826
cheikhoumou <67443473+cheikhoumou@users.noreply.github.com>
2021-03-30 23:09:19 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,108 @@
+localStorage.setItem("b", '<a href="home.html"><input type="button" value="إبدأ التعلم" ></a>');
+// function 
+function tsgil(){
+  var code = [
+    window.code1.value,
+    window.code2.value];       
+
+switch ( code[0] ){
+
+    case '1P2885549':
+        window.b1.style = "display: none;";
+        window.code1.style = "display:none;";
+       var zzz =  "<br>"+ localStorage.getItem("b");
+        window.x1.innerHTML = "تم التسجيل " + zzz;
+        localStorage.setItem('poih', 500);
+        localStorage.setItem("pin",'1P2885549');
+
+    break; // code 1
+
+    case '95462K533':
+        window.b1.style = "display: none;";
+        window.code1.style = "display:none;";
+        var zzz =  "<br>"+ localStorage.getItem("b");
+        window.x1.innerHTML = "تم التسجيل " + zzz;
+        localStorage.setItem('poih', 500);
+        localStorage.setItem("pin",'1P2885549');
+    break; // code 2
+
+    case '45146582J':
+        window.b1.style = "display: none;";
+        window.code1.style = "display:none;";
+        var zzz =  "<br>"+ localStorage.getItem("b");
+        window.x1.innerHTML = "تم التسجيل " + zzz;
+        localStorage.setItem('poih', 500);
+        localStorage.setItem("pin",'1P2885549');
+    break; // code 3
+
+    case '6522147S6':
+        window.b1.style = "display: none;";
+        window.code1.style = "display:none;";
+        var zzz =  "<br>"+ localStorage.getItem("b");
+        window.x1.innerHTML = "تم التسجيل " + zzz;
+        localStorage.setItem('poih', 500);
+        localStorage.setItem("pin",'1P2885549');
+    break;
+    default:
+        alert(' خطأ كود التفعيل غير صحيح ❌ ');
+    break;
+} 
+
+// if ( code[0].value = '45146582J' ||  '6522147S6'|| '95462K533'|| '1P2885549'){
+//     window.b1.style = "display: none;";
+//     window.code1.style = "display:none;";
+//    var zzz =  "<br>"+ localStorage.getItem("b");
+//     window.x1.innerHTML = "تم التسجيل " + zzz;
+// }else{
+//     alert(' خطأ كود التفعيل غير صحيح ❌ ');
+// }
+}
+//  This is function
+function tsgil2(){
+    localStorage.setItem("b2", '<a href="home2.html"><input type="button" value="إبدأ التعلم" ></a>');
+
+    var code = [
+        window.code1.value,
+        window.code2.value];
+    switch ( code[1] ){
+
+        case '24561789d':
+            window.b2.style = "display: none;";
+            window.code2.style = "display:none;";
+            var ccc =  "<br>"+ localStorage.getItem("b2");
+            window.x2.innerHTML = "تم التسجيل " + ccc;
+            localStorage.setItem("pin2",'24561789d');
+        break; // code 1
+    
+        case '124ccxx10':
+            window.b2.style = "display: none;";
+            window.code2.style = "display:none;";
+            var ccc =  "<br>"+ localStorage.getItem("b2");
+            window.x2.innerHTML = "تم التسجيل " + ccc;
+            localStorage.setItem("pin2",'24561789d');
+        break; // code 2
+    
+        case '78454213h':
+            window.b2.style = "display: none;";
+            window.code2.style = "display:none;";
+            var ccc =  "<br>"+ localStorage.getItem("b2");
+            window.x2.innerHTML = "تم التسجيل " + ccc;
+            localStorage.setItem("pin2",'24561789d');
+        break; // code 3
+    
+        case '31200457k':
+            window.b2.style = "display: none;";
+            window.code2.style = "display:none;";
+            var ccc =  "<br>"+ localStorage.getItem("b2");
+            window.x2.innerHTML = "تم التسجيل " + ccc;
+            localStorage.setItem("pin2",'24561789d');
+        break;
+        default:
+            alert(' خطأ كود التفعيل غير صحيح ❌ ');
+        break;
+    }  
+}
+// function masseg(){
+//     alert( " مرحبا  يا  " + localStorage.getItem("Name") + " يسعدنا هتمامك بالبرمجة وإذا أردت شراء إحدى الدورات يمكنك إرسال طلب إلينا بذلك ودفع مباشرة عبر وسائل الدفع المتوفرة وحينها ستحصل على كود تفعيل الدورة المناسبة لك  " );
+//  }
+//  setTimeout(masseg, 10000 *  7);
