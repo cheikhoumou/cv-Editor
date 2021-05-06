c04ad8290b3f559ac9b2c4c22830d6e06899e480
cheikhoumou <67443473+cheikhoumou@users.noreply.github.com>
2021-05-06 03:17:06 +0200

    app 3
    app 3

--- addsuser.js
+++ addsuser.js
@@ -7,6 +7,8 @@ document.getElementById('boxnew').innerHTML += '';
 */
 console.log('good');
 // user 1
+document.getElementById('boxnew').innerHTML += '<div class="box"><img src="https://a.top4top.io/p_1951zi2rp1.jpeg" id="imgusar"><h5 id="name"> محمد السالك أحمد زين </h5><h4 id="titletag">تعقيم المنازل</h4><p id="discription">تعقيم المنازل من الحشرات</p><h4 id="number"><img src="../img/phone.svg">36595999</h4><br><a href="https://docs.google.com/forms/d/e/1FAIpQLSeNhLuOaoV_m7C9ibksH_bhbWnNnwdWwTkEF6oDhll0qHpHSQ/viewform?embedded=true"><button>طلب خدمة</button></a></div><br>';
+// user 2
 
 
 
